624f5d13e6272d45a8710d5cfa9ac4c03829e728
AuslinPJ <31402534+AuslinPJ@users.noreply.github.com>
2017-09-27 12:56:22 +0530

    barchart

--- src/app/barchart/barchart.component.ts
+++ src/app/barchart/barchart.component.ts
@@ -19,13 +19,14 @@ export class BarchartComponent {
 data1=new Array<Chart>();
 label=new Array<any>();
 highh=new Array<any>();
+loww=new Array<any>();
   ngOnChanges(changes: SimpleChanges)
       {
     this.getValueFromChart(this.stock);
         
      }
   hData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: ['January', 'February', 'March', 'April', 'May'],
     datasets: [
       {
       }
@@ -43,21 +44,31 @@ getValueFromChart(sym : string)
    
    this.label.push(this.data1[element].date);
    this.highh.push(this.data1[element].high);
+   this.loww.push(this.data1[element].low);
   
   });console.log(this.data1);console.log(this.label);console.log(this.highh);
-  this.SetValuesAndUpdateChart(this.stock, this.label, this.highh);
+  this.SetValuesAndUpdateChart(this.stock, this.label, this.highh,this.loww);
 }
 )
  }
- SetValuesAndUpdateChart(symbol, label, highh) {
+ SetValuesAndUpdateChart(symbol, label, highh,loww) {
   this.hData.labels = label;
   this.hData.datasets[0] = {
-    label: symbol ? symbol : '',
+    //label: symbol ? symbol : '',
+    label:'high',
     data: highh,
     fill: false,
+    backgroundColor: '#42A5F5',
     borderColor: '#673ab7',
-    
-  }; this.Chart.refresh();
+      }, 
+  this.hData.datasets[1] = {
+    //label: symbol ? symbol : '',
+    label:'low',
+    data: loww,
+    fill: false,
+    backgroundColor: '#9CCC65',
+    borderColor: '#673ab7',
+  };this.Chart.refresh();
  }
   
 
